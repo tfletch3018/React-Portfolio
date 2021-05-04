11bbf5fa18b8eb0074fe23cb04d2a82c61675c50
Tonya Fletcher <tfletch4504@gmail.com>
2021-05-04 14:19:21 -0400

    routes

--- my-react-portfolio/src/App.js
+++ my-react-portfolio/src/App.js
@@ -1,20 +1,29 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/projects" component={Projects} />
+          <Route exact path="/contact" component={Contact} />
+          </Wrapper>
+          <Footer />
+          </div>
+          </Router>
   );
 }
 
 export default App;
+
+
