20c0ec46adb77cdb6e4bf925569c893c09e9a868
Tonya Fletcher <tfletch4504@gmail.com>
2021-05-05 15:56:14 -0400

    add container and grid to projects

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,14 +3,31 @@ import React from "react";
 function Contact() {
   return (
     <div>
-      <h1>About Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui
-        mauris, ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus
-        porta. Nam quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam
-        semper imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed
-        rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+      <footer class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 id="contact-me" h1 class="display-4">Contact Me</h1>
+        <ul class="nav justify-content-end">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">267-250-0212</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="#">tfletch4504@gmail.com</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active"
+              href="https://drive.google.com/drive/folders/1-2qstDNd3kGVUXQ7ZOWa-Ry3Ft4kzCaR?usp=sharing">My Resume</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="https://github.com/tfletch3018">Githhub</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="http://linkedin.com/in/tonya-fletcher-733a9450">LinkedIn</a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+
+
     </div>
   );
 }
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,14 +1,113 @@
 import React from "react";
+import Container from "../components/Container";
+import Row from "../components/Row";
+import Col from "../components/Col";
 
 function Projects() {
   return (
     <div>
-      <h1 className="text-center">hello</h1>
-      <p>
-        Lorem ipsum dolor sit amet, est ut enim consequat. Nostrum fastidii partiendo sed ne, no
-        mutat ludus aperiri mea, per in choro dolorem electram. Invidunt reprimique assueverit quo
-        ne, eruditi graecis pro ut. Usu ut diceret scaevola evertitur, appareat voluptatibus ad vel.
-      </p>
+      <Container style={{ marginTop: 30 }}>
+        <Row>
+          <Col size="md-12">
+            <h1>Welcome To Projects!</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col size="md-12">
+      <div className="col-sm-9">
+        <div class="card-first">
+          <div class="card-header">
+            Project 1: Group Collaboration
+              </div>
+          <div class="card-body">
+            <blockquote class="blockquote mb-0">
+              <p>(babysittr)</p>
+              <a href="https://github.com/tfletch3018/project-1" class="btn-project" alt="" >Projects</a>
+              <footer class="blockquote-footer">
+                {/* <figcaption class="text-center">Photo by <a
+                        // href="https://unsplash.com/@picsbyjameslee?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">James
+                        // Lee</a> on <a
+                        // href="/t/textures-patterns?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" alt="" >Unsplash</a> */}
+
+              </footer>
+            </blockquote>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-sm-6">
+          <div class="card-second">
+            <div class="card-body">
+              <h5 class="card-title">Team Profile Generator</h5>
+              <p class="card-text">JavaScript</p>
+              <a href="https://github.com/tfletch3018/Team-Profile-Generator" class="btn-project">Projects</a>
+              <footer class="blockquote-footer">
+                {/* <figcaption class="text-center">Photo by <a
+                          href="https://unsplash.com/@nickkane?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Nick
+                          Kane</a> on <a
+                          href="/t/textures-patterns?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a> */}
+
+              </footer>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-6">
+        <div class="card-third">
+          <div class="card-body">
+            <h5 class="card-title">Employee Tracker</h5>
+            <p class="card-text">JavaScript</p>
+            <a href="https://github.com/tfletch3018/Employee-Tracker" class="btn-project">Projects</a>
+            <footer class="blockquote-footer">
+              {/* <figcaption class="text-center">Photo by <a
+                          href="https://unsplash.com/@iswanto?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Iswanto
+                          Arif</a> on <a
+                          href="/t/textures-patterns?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a> */}
+            </footer>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-sm-6">
+          <div class="card-fourth">
+            <div class="card-body">
+              <h5 class="card-title">Note Taker</h5>
+              <p class="card-text">JavaScript/ CSS/ HTML</p>
+              <a href="https://github.com/tfletch3018/Note-Taker" class="btn-project">Projects</a>
+              <footer class="blockquote-footer">
+                {/* <figcaption class="text-center">Photo by <a
+                          href="https://unsplash.com/@eberhardgross?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Photo
+                          by <a
+                            href="https://unsplash.com/@cfitz?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Cody
+                            Fitzgerald</a> on <a
+                            href="/t/nature?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a> */}
+              </footer>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-6">
+        <div class="card-fifth">
+          <div class="card-body">
+            <h5 class="card-title">Weather Dashboard</h5>
+            <p class="card-text">JavaScript/ CSS/ HTML</p>
+            <a href="https://github.com/tfletch3018/Weather-Dashboard" class="btn-project">Projects</a>
+            <footer class="blockquote-footer">
+              {/* <figcaption class="text-center">Photo by <a
+                          href="https://unsplash.com/@jeisblack?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Jason
+                          Blackeye</a> on <a
+                          href="/t/nature?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a> */}
+            </footer>
+          </div>
+        </div>
+      </div>
+      </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
