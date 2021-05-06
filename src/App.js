dcfe05a7a4a1b48e34bd4b4abd5ae326607dd7ee
Tonya Fletcher <tfletch4504@gmail.com>
2021-05-06 09:39:46 -0400

    deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
