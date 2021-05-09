0299407339c1a873f904fbf74759d744e7780a89
Tonya Fletcher <tfletch4504@gmail.com>
2021-05-08 22:35:43 -0400

    style links

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,23 +5,23 @@ function Contact() {
     <div>
       <footer className="jumbotron jumbotron-fluid">
       <div className="container">
-        <h1 id="contact-me" h1 class="display-4">Contact Me</h1>
-        <ul class="nav justify-content-end">
-          <li class="nav-item">
-            <a class="nav-link active" href="https://drive.google.com/drive/folders/1-2qstDNd3kGVUXQ7ZOWa-Ry3Ft4kzCaR?usp=sharing">267-250-0212</a>
+        <h1 id="contact-me" h1 className="display-4">Contact Me</h1>
+        <ul className="nav justify-content-end">
+          <li className="nav-item">
+            <a className="nav-link active" href="https://drive.google.com/drive/folders/1-2qstDNd3kGVUXQ7ZOWa-Ry3Ft4kzCaR?usp=sharing">267-250-0212</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link active" href="https://drive.google.com/drive/folders/1-2qstDNd3kGVUXQ7ZOWa-Ry3Ft4kzCaR?usp=sharing">tfletch4504@gmail.com</a>
+          <li className="nav-item">
+            <a className="nav-link active" href="https://drive.google.com/drive/folders/1-2qstDNd3kGVUXQ7ZOWa-Ry3Ft4kzCaR?usp=sharing">tfletch4504@gmail.com</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link active"
+          <li className="nav-item">
+            <a className="nav-link active"
               href="https://drive.google.com/drive/folders/1-2qstDNd3kGVUXQ7ZOWa-Ry3Ft4kzCaR?usp=sharing">My Resume</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link active" href="https://github.com/tfletch3018">Githhub</a>
+          <li className="nav-item">
+            <a className="nav-link active" href="https://github.com/tfletch3018">Githhub</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link active" href="http://linkedin.com/in/tonya-fletcher-733a9450">LinkedIn</a>
+          <li className="nav-item">
+            <a className="nav-link active" href="http://linkedin.com/in/tonya-fletcher-733a9450">LinkedIn</a>
           </li>
         </ul>
       </div>
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -6,7 +6,7 @@ import Col from "../components/Col";
 function Projects() {
   return (
     <div>
-<h1 class="text-center">Projects</h1>
+<h1 className="text-center">Projects</h1>
       <Container style={{ marginTop: 30 }}>
         <Row>
           <Col size="md-12">
@@ -15,94 +15,94 @@ function Projects() {
         <Row>
           <Col size="md-12">
             <div className="card-first" >
-              <div class="card-header">
+              <div className="card-header">
                 Project 1: Group Collaboration
               </div>
               <div className="card-body">
-                <blockquote class="blockquote mb-0">
+                <blockquote className="blockquote mb-0">
                   <p>(babysittr)</p>
-                  <a href="https://github.com/tfletch3018/project-1" class="btn-project" alt="" >Github repo</a>
-                  <a href="https://polysnacktyl.github.io/project-1/" class="btn-project" alt="" >Deployed application</a>
-                  <footer class="blockquote-footer">
-                    <figcaption class="text-center">Photo by
-                <a href="https://unsplash.com/@picsbyjameslee?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" alt="" >James Lee</a> on
-                <a href="/t/textures-patterns?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" alt="" >Unsplash</a>
+                  <a href="https://github.com/tfletch3018/project-1" className="btn-project" alt="" >Github repo</a>
+                  <a href="https://polysnacktyl.github.io/project-1/" className="btn-project" alt="" >Deployed application</a>
+                  <footer className="blockquote-footer">
+                    <figcaption className="text-center">Photo by
+                <a className="links" href="https://unsplash.com/@picsbyjameslee?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" alt="" >James Lee</a> on
+                <a className="links" href="/t/textures-patterns?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" alt="" >Unsplash</a>
                     </figcaption>
                   </footer>
                 </blockquote>
               </div>
             </div>
 
-            <div class="card-second">
-              <div class="card-body">
-                <h5 class="card-title">Workout Tracker</h5>
-                <p class="card-text">JavaScript/ CSS/ HTML</p>
-                <a href="https://github.com/tfletch3018/Workout-Tracker" class="btn-project">Github repo</a>
-                <a href="https://sleepy-fortress-48942.herokuapp.com/?id=6088694a92342e0015fb5d32" class="btn-project">Deployed application</a>
-                <footer class="blockquote-footer">
-                  <figcaption class="text-center">Photo by
-                <a href="https://unsplash.com/@nickkane?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Nick Kane</a> on
-                <a href="/t/textures-patterns?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+            <div className="card-second">
+              <div className="card-body">
+                <h5 className="card-title">Workout Tracker</h5>
+                <p className="card-text">JavaScript/ CSS/ HTML</p>
+                <a href="https://github.com/tfletch3018/Workout-Tracker" className="btn-project">Github repo</a>
+                <a href="https://sleepy-fortress-48942.herokuapp.com/?id=6088694a92342e0015fb5d32" className="btn-project">Deployed application</a>
+                <footer className="blockquote-footer">
+                  <figcaption className="text-center">Photo by
+                <a className="links" href="https://unsplash.com/@nickkane?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Nick Kane</a> on
+                <a className="links" href="/t/textures-patterns?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
                   </figcaption>
                 </footer>
               </div>
             </div>
 
-            <div class="card-third">
-              <div class="card-body">
-                <h5 class="card-title">Project 2: Groupie</h5>
-                <p class="card-text">CSS/ JavaScript/ Handlebars/ HTML</p>
-                <a href="https://github.com/tfletch3018/Project-2" class="btn-project">Github repo</a>
-                <a href="http://sleepy-headland-27879.herokuapp.com/" class="btn-project">Deployed application</a>
-                <footer class="blockquote-footer">
-                  <figcaption class="text-center">Photo by 
-                  <a href="https://unsplash.com/@t_lipke?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Thomas Lipke</a> on 
-                  <a href="https://unsplash.com/s/photos/red?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+            <div className="card-third">
+              <div className="card-body">
+                <h5 className="card-title">Project 2: Groupie</h5>
+                <p className="card-text">CSS/ JavaScript/ Handlebars/ HTML</p>
+                <a href="https://github.com/tfletch3018/Project-2" className="btn-project">Github repo</a>
+                <a href="http://sleepy-headland-27879.herokuapp.com/" className="btn-project">Deployed application</a>
+                <footer className="blockquote-footer">
+                  <figcaption className="text-center">Photo by 
+                  <a className="links" href="https://unsplash.com/@t_lipke?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Thomas Lipke</a> on 
+                  <a className="links" href="https://unsplash.com/s/photos/red?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
                   </figcaption>
                 </footer>
               </div>
             </div>
 
-            <div class="card-fourth">
-              <div class="card-body">
-                <h5 class="card-title">Note Taker</h5>
-                <p class="card-text">JavaScript/ CSS/ HTML</p>
-                <a href="https://github.com/tfletch3018/Note-Taker" class="btn-project">Github repo</a>
-                <a href="https://tfletchnote-taker.herokuapp.com/" class="btn-project">Deployed application</a>
-                <footer class="blockquote-footer">
-                  <figcaption class="text-center">Photo by
-                    <a href="https://unsplash.com/@eberhardgross?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Photo by
-                    <a href="https://unsplash.com/@cfitz?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Cody Fitzgerald</a> on <a href="/t/nature?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></a>
+            <div className="card-fourth">
+              <div className="card-body">
+                <h5 className="card-title">Note Taker</h5>
+                <p className="card-text">JavaScript/ CSS/ HTML</p>
+                <a href="https://github.com/tfletch3018/Note-Taker" className="btn-project">Github repo</a>
+                <a href="https://tfletchnote-taker.herokuapp.com/" className="btn-project">Deployed application</a>
+                <footer className="blockquote-footer">
+                  <figcaption className="text-center">Photo by
+                    <a className="links" href="https://unsplash.com/@cfitz?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Cody Fitzgerald</a> on 
+                    <a className="links" href="/t/nature?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
                   </figcaption>
                 </footer>
               </div>
             </div>
 
-            <div class="card-fifth">
-              <div class="card-body">
-                <h5 class="card-title">Weather Dashboard</h5>
-                <p class="card-text">JavaScript/ CSS/ HTML</p>
-                <a href="https://github.com/tfletch3018/Weather-Dashboard" class="btn-project">Github repo</a>
-                <a href="https://tfletch3018.github.io/Weather-Dashboard/" class="btn-project">Deployed application</a>
-                <footer class="blockquote-footer">
-                   <figcaption class="text-center">Photo by 
-                   <a href="https://unsplash.com/@ej1209_?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">EJ Li</a> on 
-                   <a href="https://unsplash.com/s/photos/orchids?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>   
+            <div className="card-fifth">
+              <div className="card-body">
+                <h5 className="card-title">Weather Dashboard</h5>
+                <p className="card-text">JavaScript/ CSS/ HTML</p>
+                <a href="https://github.com/tfletch3018/Weather-Dashboard" className="btn-project">Github repo</a>
+                <a href="https://tfletch3018.github.io/Weather-Dashboard/" className="btn-project">Deployed application</a>
+                <footer className="blockquote-footer">
+                   <figcaption className="text-center">Photo by 
+                   <a className="links" href="https://unsplash.com/@ej1209_?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">EJ Li</a> on 
+                   <a className="links" href="https://unsplash.com/s/photos/orchids?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>   
                   </figcaption>
                 </footer>
               </div>
             </div>
 
-            <div class="card-sixth">
-              <div class="card-body">
-                <h5 class="card-title">Offline Budget Trackers</h5>
-                <p class="card-text">JavaScript/ HTML/ CSS</p>
-                <a href="https://github.com/tfletch3018/Offline-Budget-Trackers" class="btn-project">Github repo</a>
-                <a href="https://agile-bastion-05761.herokuapp.com/" class="btn-project">Deployed application</a>
-                <footer class="blockquote-footer">
-                  <figcaption class="text-center">Photo by
-                        <a href="https://unsplash.com/@stavrialena?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Stavrialena Gontzou</a> on
-                        <a href="https://unsplash.com/t/textures-patterns?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+            <div className="card-sixth">
+              <div className="card-body">
+                <h5 className="card-title">Offline Budget Trackers</h5>
+                <p className="card-text">JavaScript/ HTML/ CSS</p>
+                <a href="https://github.com/tfletch3018/Offline-Budget-Trackers" className="btn-project">Github repo</a>
+                <a href="https://agile-bastion-05761.herokuapp.com/" className="btn-project">Deployed application</a>
+                <footer className="blockquote-footer">
+                  <figcaption className="text-center">Photo by
+                        <a className="links" href="https://unsplash.com/@stavrialena?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Stavrialena Gontzou</a> on
+                        <a className="links" href="https://unsplash.com/t/textures-patterns?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
                   </figcaption>
                 </footer>
               </div>
