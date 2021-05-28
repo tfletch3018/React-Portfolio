f07bfb623dae5be14d7461a0abb34afa79a741fd
Tonya Fletcher <tfletch4504@gmail.com>
2021-05-27 22:10:41 -0400

    add skills

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,35 +4,49 @@ import profileImage from "../images/profileImage.jpg";
 function Home() {
   return (
     <div>
-    <div className="media">
-      <div className="media-left">
-        <img src={profileImage} className="media-object img-thumbnail"  alt="" />
-      </div>
-      <div className="media-body">
-        <div>
-          
+      <div className="media">
+        <div className="media-left">
+          <img src={profileImage} className="media-object img-thumbnail" alt="" />
         </div>
-        <div className="about">I am a former service and hospitality industry professional and manager excited about my entry into the field
-          of web development. I am currently a student enrolled in an online coding bootcamp hosted by the University of
-          Pennsylvania. I live in Philadelphia, PA, where I have resided since graduating from Temple University some
-          time ago. After being laid off as a result of COVID19 in 2020, I quickly realized that the world was changing
-          with regard to how people conduct business, and that it was time for me to change as well. Gone are the days
-          of handshakes and face to face interactions, replaced by Zoom meetings and “contact-less” deliveries. I am
-          hoping to find an opportunity that enables me to utilize my strong organizational and planning skills attained
-          from my years as a customer service manager, as well as my creativity and desire to be challenged by a new
-          endeavor.
-          I have been playing the piano since I was young, and really enjoy doing so in my spare time. And if I am not
-          attempting to crack some code, I am probably taking a run, trying out a new recipe in the kitchen, or just
-          hanging around with my two cats Lynx and Malley.
-          .</div>
+        <div className="media-body">
+          <div>
+
+          </div>
+          <div className="about">
+            <h1 className="home">About Me</h1>
+            <p className="about-me">
+              I am a former hospitality and service industry professional with a background in Business Administration, and
+              recent graduate of the Full-Stack Bootcamp hosted by the University of Pennsylvania. Effective at utilizing
+              the strong organizational and planning skills attained from my experience as a customer service manager, as well as
+              my creativity and desire to be challenged by a new endeavor.  Known to have an incredibly strong work ethic and the
+              ability to place intense focus on the task at hand with the goal of seeing it through to completion.
+          </p>
+            <p className="about-me">
+              I have been playing the piano since I was young, and really enjoy doing so in my spare time. And if I am not
+              attempting to crack some code, I am probably taking a run, trying out a new recipe in the kitchen, or just
+              hanging around with my two cats Lynx and Malley.
+          </p>
+          </div>
+
+          <div className="about">
+            <h1 className="home">Skills/ Technologies</h1>
+            <ul className="skills">HTML</ul>
+            <ul className="skills">CSS</ul>
+            <ul className="skills">JavaScript</ul>
+            <ul className="skills">NodeJS</ul>
+            <ul className="skills">Git</ul>
+            <ul className="skills">Express</ul>
+            <ul className="skills">jQuery</ul>
+            <ul className="skills">Bootstrap</ul>
+            <ul className="skills">React</ul>
+            <ul className="skills">MySQL</ul>
+            <ul className="skills">NoSQL</ul>
+            <ul className="skills">MongoDB</ul>
+          </div>
+        </div>
       </div>
     </div>
-      
 
-
-
-
-    </div>
   );
 }
 
